35f1d12b6000c20d369a132e29cbb0ac0d1f2816
web-collab <tanmoy587@gmail.com>
2024-07-22 16:11:01 +0530

    2.@postMapping , @RequestParam

--- src/main/java/org/employeesheet/employeesheet/MyController.java
+++ src/main/java/org/employeesheet/employeesheet/MyController.java
@@ -1,6 +1,7 @@
 package org.employeesheet.employeesheet;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,12 @@ public class MyController {
 
     @GetMapping("process-form")
     public static String getData(@RequestParam int num1, @RequestParam int num2) {
+        int res = num1+num2;
+        return String.valueOf(res);
+    }
+
+    @PostMapping("process-form")
+    public static String postData(@RequestParam int num1, @RequestParam int num2) {
         int res = num1+num2;
         return String.valueOf(res);
     }
